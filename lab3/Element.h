825f952fdf4e353b1e8b29a5ab43592892308c22
devanshrj <devanshrj@outlook.com>
2021-02-05 17:08:38 +0530

    Lab 3 exercise 1.

---
+++ lab3/Element.h
@@ -0,0 +1,3 @@
+/* Element Header */
+
+typedef int Element;
---
+++ lab3/driver.c
@@ -0,0 +1,39 @@
+/* Driver for Queue */
+
+#include "que.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    // Create a file pointer and open the file read as command line argument.
+    FILE *fileptr = fopen(argv[1], "r");
+
+    // Declare a pointer to a stack (top) and allocate memory to it.
+    struct Queue *q = newQ();
+    
+    // stack insertion
+    while (!feof(fileptr))
+    {
+        // read the next element and store into the temp variable.
+        int temp;
+        fscanf(fileptr, "%d ", &temp);
+        // printf("Element: %d\n", temp);
+        // insert temp into the linked list.
+        addQ(q, temp);
+    }
+    // close the file pointer
+    fclose(fileptr);
+
+    // print the stack.
+    printf("Queue count: %d", lengthQ(q));
+    printQ(q);
+
+    // pop elements
+    printf("\nRemoving elements from the queue...\n");
+    while (q->count)
+    {
+        printf("Element popped: %d\n", front(q));
+        q = delQ(q);
+    }
+    // printf("Element popped: %d\n", front(q));
+}
